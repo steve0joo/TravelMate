21c2d5a536fd7f6a43bd5ddc62ef30698d08a5f9
IronhawkReigns <yejoons_2026@gatech.edu>
2025-04-08 16:58:24 -0400

    Added trips list go back function + delete trips

--- trips/urls.py
+++ trips/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('new/', views.create_trip, name='create_trip'),
     path('list/', views.trip_list, name='trip_list'),
+    path('<int:trip_id>/', views.trip_detail, name='trip_detail'),
+    path('<int:trip_id>/delete/', views.delete_trip, name='delete_trip'),
 ]
--- trips/views.py
+++ trips/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Trip
 from .forms import TripForm
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+@login_required
+def delete_trip(request, trip_id):
+    trip = get_object_or_404(Trip, id=trip_id, user=request.user)
+    
+    if request.method == 'POST':
+        trip.delete()
+        messages.success(request, 'Trip deleted successfully.')
+        return redirect('trip_list')
+    
+    return render(request, 'trips/confirm_delete.html', {'trip': trip})
 
 @login_required
 def create_trip(request):
@@ -20,3 +35,8 @@ def create_trip(request):
 def trip_list(request):
     trips = Trip.objects.filter(user=request.user).order_by('-start_date')
     return render(request, 'trips/trip_list.html', {'trips': trips})
+
+@login_required
+def trip_detail(request, trip_id):
+    trip = get_object_or_404(Trip, id=trip_id, user=request.user)
+    return render(request, 'trips/trip_detail.html', {'trip': trip})
