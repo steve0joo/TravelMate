ea6a01ab2fb265bb01436c5a7c5952a066e32da0
DavidymLee <davidleeym03@gmail.com>
2025-04-10 14:31:26 -0400

    Reapply "Username and Password filed name inside login page"
    This reverts commit 412104d031015a3c55f5122b7153786a5ca88a0a.

---
+++ trips/urls 2.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('new/', views.create_trip, name='create_trip'),
+    path('list/', views.trip_list, name='trip_list'),
+    path('<int:trip_id>/', views.trip_detail, name='trip_detail'),
+    path('<int:trip_id>/delete/', views.delete_trip, name='delete_trip'),
+    path('<int:trip_id>/packing/', views.packing_list, name='packing_list'),
+]
