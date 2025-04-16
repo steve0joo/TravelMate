f2cd8e0f1211ead3ebe503c3804fdc03ee8f16f6
Jack <jackthor0608@gmail.com>
2025-04-15 22:31:44 -0400

    home app working

--- travelmate/urls.py
+++ travelmate/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('trips/', include('trips.urls')),
