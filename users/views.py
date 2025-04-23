d729a48a6d0ef4b572f900a71e9c2aa0385f8f4e
IronhawkReigns <yejoons_2026@gatech.edu>
2025-04-22 23:40:09 -0400

    Account Info Update (username, PW) function added

--- users/views.py
+++ users/views.py
@@ -5,6 +5,43 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib.auth import get_backends
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from .forms import UserResetForm, CustomPasswordChangeForm
+from allauth.socialaccount.models import SocialAccount
+
+@login_required
+def reset_user_view(request):
+    is_social_user = request.user.socialaccount_set.exists()
+    can_change_password = request.user.has_usable_password()
+
+    user_form = UserResetForm(instance=request.user)
+    password_form = CustomPasswordChangeForm(user=request.user) if can_change_password else None
+
+    if request.method == 'POST':
+        user_form = UserResetForm(request.POST, instance=request.user)
+        if can_change_password:
+            password_form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+
+        valid = user_form.is_valid()
+        if can_change_password:
+            valid = valid and password_form.is_valid()
+
+        if valid:
+            user_form.save()
+            if can_change_password:
+                password_form.save()
+                update_session_auth_hash(request, request.user)
+            return redirect('profile')
+
+    return render(request, 'users/reset_user.html', {
+        'user_form': user_form,
+        'password_form': password_form,
+        'is_social_user': is_social_user,
+        'can_change_password': can_change_password,
+    })
+
+
 
 def signup_view(request):
     if request.method == 'POST':
