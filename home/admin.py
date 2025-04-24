ac4d25509249f7b862ca52a4aafec5d3fc9a2e8b
DavidymLee <davidleeym03@gmail.com>
2025-04-23 22:53:27 -0400

    Redesign for reset_user.html

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Register the built-in User model
+admin.site.register(User, UserAdmin)
+
+# Optionally register EmailAddress if not already done by allauth
+try:
+    from allauth.account.models import EmailAddress
+    from allauth.account.admin import EmailAddressAdmin
+    admin.site.register(EmailAddress, EmailAddressAdmin)
+except admin.sites.AlreadyRegistered:
+    pass
