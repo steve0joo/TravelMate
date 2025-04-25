eae68a1d13ce61f77f0d29a5071c8ff3630d7e52
Jack <jackthor0608@gmail.com>
2025-04-24 20:00:00 -0400

    reset password feature

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,6 +8,38 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
-    path('reset/', views.reset_user_view, name='reset_user'),
+    path('profile/edit/', views.reset_user_view, name='reset_user'),
     path('spotlight/', views.spotlight_view, name='spotlight'),
+# 1) Show form to enter email
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
+        name='password_reset'
+    ),
+    # 2) “Check your inbox” page
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    # 3) Link landed on from email
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    # 4) All done
+    path(
+        'password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
 ]
